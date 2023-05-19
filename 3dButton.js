e047d2c651b37fe14fb93e0c94d5d8060e3a113c
Maksymilian Karol Ludwik Mazur <81038515+YEETGeneral@users.noreply.github.com>
2023-05-19 21:44:22 +0200

    Delete 3dButton.js

--- 3dButton.js
+++
@@ -1,8 +0,0 @@
-const img = document.createElement('img');
-const imgContainer = document.querySelector('.rightContainer');
-
-imgContainer.addEventListener('mousemove', (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    img.style.transform = `rotateY(${yAxis}deg) rotateX$({xAxis}deg)`;
-}); 
